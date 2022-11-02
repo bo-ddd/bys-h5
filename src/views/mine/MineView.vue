5925f4a7c298800343d5d5b177fee904d9f4f20e
zhujiangyan <t2710086269@163.com>
2022-11-02 20:29:18 +0800

    main.css wrap

--- src/views/mine/MineView.vue
+++ src/views/mine/MineView.vue
@@ -1,10 +1,38 @@
 <template>
-    <div class="">
-      
-    </div>
-  </template>
+  <div class="">
+    <header class="wrap mt-38 header c-ffffff">
+      <p class="fs-22 fw-600">请创建简历</p>
+      <span class="fs-12">点击头像可编辑</span>
+    </header>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
   
-  <style lang="scss" scoped>
-  
-  </style>
+<style lang="scss" scoped>
+.fs-12{
+  font-size: 1.2rem;
+}
+.fs-22{
+  font-size: 2.2rem;
+}
+.fw-600{
+  font-weight: 600;
+}
+
+.c-ffffff{
+  color: #ffffff;
+}
+
+.mt-38{
+  margin-top: 3.8rem;
+}
+
+.header{
+  background-color: #3472e1;
+}
+
+</style>
   

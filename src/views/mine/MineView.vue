aacbb0f3e13998ea356713fa6eea35125a34e921
zhujiangyan <t2710086269@163.com>
2022-11-02 20:37:46 +0800

    updata wrap

--- src/views/mine/MineView.vue
+++ src/views/mine/MineView.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="">
-    <header class="wrap mt-38 header c-ffffff">
-      <p class="fs-22 fw-600">请创建简历</p>
-      <span class="fs-12">点击头像可编辑</span>
+    <header class="wrap header c-ffffff">
+      <div class="container">
+        <div class="title">
+          <p class="fs-22 fw-600">请创建简历</p>
+          <span class="fs-12">点击头像可编辑</span>
+        </div>
+        <div class="upload">
+
+        </div>
+      </div>
     </header>
   </div>
 </template>
@@ -12,27 +19,33 @@
 </script>
   
 <style lang="scss" scoped>
-.fs-12{
+.fs-12 {
   font-size: 1.2rem;
 }
-.fs-22{
+
+.fs-22 {
   font-size: 2.2rem;
 }
-.fw-600{
+
+.fw-600 {
   font-weight: 600;
 }
 
-.c-ffffff{
+.c-ffffff {
   color: #ffffff;
 }
 
-.mt-38{
+.mt-38 {
   margin-top: 3.8rem;
 }
 
-.header{
+.header {
   background-color: #3472e1;
-}
 
+  .container{
+    padding: 3.8rem 0;
+
+  }
+}
 </style>
   

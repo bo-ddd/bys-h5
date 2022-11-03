a641aacda6cb1ab2cc8a88475108b52d7dca0584
shangyuhao <834725611@qq.com>
2022-11-03 09:12:29 +0800

    1

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Layout from '@/views/layout/Layout.vue'
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -31,6 +30,11 @@ const router = createRouter({
         }
       ]
     },
+    {
+      path:"/deliveryfeedback",
+      name:"deliveryfeedback",
+      component:() => import('@/views/deliveryfeedback/deliveryfeedback.vue')
+    }
   ]
 })
 

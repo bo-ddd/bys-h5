47a4053bd80da36c1d94f677c1e065f3899d57b9
shangyuhao <834725611@qq.com>
2022-11-03 16:47:47 +0800

    feedback

---
+++ src/views/feedBack/feedBack.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="feed-back">
+          <div class="wrap">
+               <div class="title">
+                <h2>请选择反馈类型</h2>
+               </div>
+               <div class="main">
+                <div class="main-item mt-28 color-180180">
+                    向开发者反馈
+                </div>
+                <div class="main-item">
+                    <h3 class="pt-8">
+                        功能异常
+                    </h3>
+                    <span class="color-180180">
+                        请向开发者反馈小程序功能异常问题。
+                    </span>
+                </div>
+
+                <div class="main-item">
+                    <h3 class="pt-8">
+                        产品建议
+                    </h3>
+                    <span class="color-180180">
+                        请向开发者反馈你对产品的相关建议。
+                    </span>
+                </div>
+
+                <div class="main-item mt-28 color-180180">
+                    向微信平台投诉
+                </div>
+
+                <div class="main-item">
+                    <h3 class="pt-8">
+                       违规举报
+                    </h3>
+                    <span class="color-180180">
+                        若遇色情、诱导、骚扰、欺诈、恶意营销、违法犯罪等情况，可向微信举报。
+                    </span>
+                </div>
+               </div>
+          </div>
+    </div>
+</template>
+  
+<script setup lang="ts">
+import { ref } from "vue";
+
+</script>
+    
+<style lang="scss" scoped>
+  
+.feed-back{
+    h3{
+                font-weight: 500;
+                color: black;
+            }
+    .pt-8{
+        padding:.8rem 0;
+    }
+    .mt-28{
+        margin-top: 2.8rem;
+    }
+    .color-180180{
+        color: rgb(127, 127, 127);
+    }
+    padding:0 1.2rem;
+    .wrap{ 
+        .main-item{
+            border-bottom: .1rem solid rgb(223, 223, 223);
+            padding-bottom: 1.2rem;
+            font-weight: none;
+        }
+        margin:14rem auto;
+        .title{
+            text-align: center;
+        }
+    }
+}
+</style>
+    

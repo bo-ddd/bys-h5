916fd26750c1a3e9f64d085fc0557ebc0fc9ecb3
shangyuhao <834725611@qq.com>
2022-11-06 21:33:27 +0800

    1

--- src/views/jobfairs/JobFairsView.vue
+++ src/views/jobfairs/JobFairsView.vue
@@ -6,6 +6,7 @@
 
     <div class="main">
       <div class="banner-box"></div>
+
       <div class="data-statistics">
         <div class="filx-column_center">
           <div>503</div>
@@ -38,14 +39,8 @@
       </div>
 
       <div class="content">
-        <div class="popularPositions">
-          <div class="popularPositions-header">
-            <div>
-              <div class="beam"></div>
-            </div>
-            <div>热门职位</div>
-          </div>
-          <div class="swiper-box">
+        <JobFairsNav title="热门职位">
+          <template #content>
             <van-swipe class="my-swipe" :autoplay="4000" indicator-color="white">
               <van-swipe-item class="ptl-20">
                 <div class="swipe-item_box">
@@ -88,15 +83,110 @@
                 </div>
               </van-swipe-item>
             </van-swipe>
-          </div>
-        </div>
+          </template>
+        </JobFairsNav>
+
+        <JobFairsNav title="招聘企业" class="mt-38">
+          <template #header>
+            <span class="fs-14 c-747474">查看全部 ></span>
+          </template>
+          <template #content>
+            <van-swipe class="my-swipe" :autoplay="4000" indicator-color="white">
+              <van-swipe-item class="ptl-20">
+                <div class="swipe-enterprise_box">
+                  <div class="just-between">
+                    <div><img src="@/assets/images/icon-invitation.png" alt=""></div>
+                    <div><img src="@/assets/images/icon-invitation.png" alt=""></div>
+                    <div><img src="@/assets/images/icon-invitation.png" alt=""></div>
+                    <div><img src="@/assets/images/icon-invitation.png" alt=""></div>
+                  </div>
+                  <div class="just-between">
+                    <div><img src="@/assets/images/icon-invitation.png" alt=""></div>
+                    <div><img src="@/assets/images/icon-invitation.png" alt=""></div>
+                    <div><img src="@/assets/images/icon-invitation.png" alt=""></div>
+                    <div><img src="@/assets/images/icon-invitation.png" alt=""></div>
+                  </div>
+                </div>
+              </van-swipe-item>
+              <van-swipe-item class="ptl-20">
+                <div class="swipe-enterprise_box">
+                  <div class="just-between">
+                    <div><img src="@/assets/images/icon-invitation.png" alt=""></div>
+                    <div><img src="@/assets/images/icon-invitation.png" alt=""></div>
+                    <div><img src="@/assets/images/icon-invitation.png" alt=""></div>
+                    <div><img src="@/assets/images/icon-invitation.png" alt=""></div>
+                  </div>
+                  <div class="just-between">
+                    <div><img src="@/assets/images/icon-invitation.png" alt=""></div>
+                    <div><img src="@/assets/images/icon-invitation.png" alt=""></div>
+                    <div><img src="@/assets/images/icon-invitation.png" alt=""></div>
+                    <div><img src="@/assets/images/icon-invitation.png" alt=""></div>
+                  </div>
+                </div>
+              </van-swipe-item>
+            </van-swipe>
+          </template>
+        </JobFairsNav>
+
+        <JobFairsNav title="招聘职位" class="mt-38">
+          <template #content>
+            <div class="position-box">
+              <div :class="isOpen ? 'is-item-box' : 'item-box'">
+                <div class="item">淘宝职位</div>
+                <div class="item">淘宝职位</div>
+                <div class="item">淘宝职位</div>
+                <div class="item">淘宝职位</div>
+                <div class="item">淘宝职位</div>
+                <div class="item">淘宝职位</div>
+                <div class="item">淘宝职</div>
+                <div class="item">淘宝职位</div>
+                <div class="item">淘</div>
+                <div class="item">淘宝职位</div>
+                <div class="item">淘宝职位</div>
+                <div class="item">淘宝职位</div>
+                <div class="item">淘宝职位</div>
+                <div class="item">淘宝职位</div>
+                <div class="item">淘宝职位</div>
+                <div class="item">淘宝职位</div>
+                <div class="item">淘宝职位</div>
+                <div class="item">淘宝职</div>
+                <div class="item">淘宝职位</div>
+                <div class="item">淘</div>
+                <div class="item">淘宝职位</div>
+                <div class="item">淘宝职位</div>
+                <div class="item">淘宝职位</div>
+                <div class="item">淘宝职位</div>
+                <div class="item">淘宝职位</div>
+                <div class="item">淘宝职位</div>
+                <div class="item">淘宝职位</div>
+                <div class="item">淘宝职位</div>
+                <div class="item">淘宝职</div>
+                <div class="item">淘宝职位</div>
+                <div class="item">淘</div>
+                <div class="item">淘宝职位</div>
+                <div class="item">淘宝职位</div>
+                <div class="item">淘宝职位</div>
+              </div>
+              <div class="is-open">
+                <span  @click="isOpenFn">{{isOpen ? 'close' : "open"}}</span>
+              </div>
+            </div>
+          </template>
+        </JobFairsNav>
+
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
+import JobFairsNav from "@/components/jobfairs/JobFairsNav.vue";
+let isOpen = ref(false);
 
+let isOpenFn = ()=>{
+  isOpen.value =  isOpen.value == true ? false : true; 
+}
 </script>
 
 <style lang="scss" scoped>
@@ -105,19 +195,6 @@
 
   .pt-6 {
     padding-top: .6rem;
-  }
-
-  .swipe-item_box {
-    display: flex;
-    gap: 2rem 0;
-    justify-content: space-between;
-    width: 90%;
-    margin: 0 auto;
-
-    img {
-      width: 4rem;
-      height: 4rem;
-    }
   }
 
   .filx-column_center {
@@ -141,24 +218,55 @@
   .main {
     height: calc(100% - 7rem);
     overflow: auto;
-    background: #F8F8F8;
+    background: #ffffff;
 
     .content {
       margin-top: 7rem;
       padding: 0 2rem;
-    }
 
-    .popularPositions {
-      .popularPositions-header {
-        display: flex;
-        font-size: 1.9rem;
-        align-items: center;
-        gap: 0 .6rem;
+      .position-box {
+        .is-open{
+          text-align: center;
+          margin-top: 2rem;
+          font-size: 1.6rem;
+          color: #acacac;
+        }
+       .is-item-box {
+          margin-top:1.8rem;
+          display: flex;
+          flex-wrap: wrap;
+          align-items: center;
+          gap:1.4rem 2.8rem;
+          .item {
+            text-align: center;
+            width: 6.2rem;
+            font-size: 1.4rem;
+          }
+        }
+        .item-box {
+          margin-top:1.8rem;
+          display: flex;
+          flex-wrap: wrap;
+          overflow: hidden;
+          height:15rem;
+          align-items: center;
+          gap:1.4rem 2.8rem;
+          .item {
+            text-align: center;
+            width: 6.2rem;
+            font-size: 1.4rem;
+          }
+        }
 
-        .beam {
-          width: .4rem;
-          background: blue;
-          height: 1.9rem;
+      }
+
+      .swipe-enterprise_box {
+        margin: 0 auto;
+        width: 95%;
+
+        img {
+          width: 7rem;
+          height: 7rem;
         }
       }
 
@@ -166,6 +274,20 @@
         font-size: 1.4rem;
         margin-top: 2rem;
         padding-bottom: 2rem;
+
+        .swipe-item_box {
+          img {
+            width: 4rem;
+            height: 4rem;
+          }
+
+          display: flex;
+          gap: 2rem 0;
+          justify-content: space-between;
+          width: 90%;
+          margin: 0 auto;
+          text-align: center;
+        }
       }
     }
 
@@ -192,6 +314,7 @@
       justify-content: space-between;
       padding: 1.6rem 2rem;
       font-size: 1.4rem;
+      box-shadow: 0rem .4rem 0rem 0rem rgb(240, 240, 240);
       z-index: 999;
     }
   }
@@ -203,7 +326,7 @@
 
 :deep(.van-swipe__indicator) {
   background: rgb(154, 154, 154);
- 
+
 }
 
 :deep(.van-swipe__indicator--active) {

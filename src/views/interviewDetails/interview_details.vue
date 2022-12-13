8d0d28853441c4da222117ee8ff07bc0ccbd7a22
lzp885387491 <885387491@qq.com>
2022-12-13 09:39:29 +0800

    Update code

--- src/views/interviewDetails/interview_details.vue
+++ src/views/interviewDetails/interview_details.vue
@@ -2,19 +2,28 @@
     <div class="interview_details">
         <div class="header">
             <div>
-                <div><img src="@/assets/images/icon-avater1.png" alt=""></div>
-                <div>
-                    <div>中科百谷</div>
-                    <h2>软件测试工程师</h2>
-                </div>
-                <div>
-                    <van-icon name="arrow" />
+                <div class="header_box align-center">
+                    <div class="companyLogo ml-23">
+                        <img src="@/assets/images/icon-avater1.png" alt="">
+                    </div>
+                    <div class="just-between header_box-center">
+                        <div>
+                            <div>
+                                <div class="fs-14">中科百谷</div>
+                                <h2>软件测试工程师</h2>
+                            </div>
+                            <div class="fs-1">北京-北京市-昌平区 | 8-12k</div>
+                        </div>
+                        <div class="icon_arrow">
+                            <van-icon name="arrow" />
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div>北京-北京市-昌平区 | 8-12k</div>
+            <div class="container">
+
+            </div>
         </div>
-        <div class="container"></div>
-        <div class="footer"></div>
     </div>
 </template>
 
@@ -61,17 +70,68 @@ async function getUserInterview() {
         userId: 10000,
     });
     if (res.data) {
-        Object.assign(interviewData, res.data);
+        res.data.forEach((item: any) => {
+            if (item.companyId == companyId.value) {
+                Object.assign(interviewData, item);
+            }
+        });
+        console.log(interviewData);
     }
 };
 </script>
 
 <style scoped lang="scss">
+.ml-23 {
+    margin-left: 2.3rem;
+}
+
+.fs-14 {
+    font-size: 1.4rem;
+}
+
+.fs-1 {
+    font-size: 1rem;
+}
+
 .interview_details {
     .header {
         width: 100vw;
-        height: 11.7rem;
+        height: 100vh;
         background-color: #5893fc;
+
+        .header_box {
+            height: 11.7rem;
+            ;
+
+            .companyLogo,
+            img {
+                width: 4.6rem;
+                height: 4.6rem;
+                border: .1rem solid #ccc;
+            }
+
+            .header_box-center {
+                color: #fff;
+                width: 100%;
+                padding-left: .7rem;
+
+                h2 {
+                    padding: .5rem 0;
+                    font-size: 1.7rem;
+                }
+            }
+
+            .icon_arrow {
+                padding-right: 2.4rem;
+            }
+        }
+    }
+
+    .container {
+        width: 100%;
+        height: calc(100vh - 11.7rem);
+        border-radius: 2rem 2rem 0 0;
+        background-color: #fff;
     }
 }
 </style>
